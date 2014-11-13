de2b59ff4f5b0974a9018146fc19dc6b318bf9d4
ChadAtGrove <chad@grovelabs.io>
2014-11-13 16:10:14 -0500

    Update and rename Adafruit_PWMServoDriver.cpp to GroveLabs_I2C_PWM.cpp

--- firmware/GroveLabs_I2C_PWM.cpp
+++ firmware/GroveLabs_I2C_PWM.cpp
@@ -19,9 +19,9 @@
 #if defined (SPARK)
  #define WIRE Wire
  #include <math.h>
- #include "Adafruit_PWMServoDriver.h"
+ #include "GroveLabs_I2C_PWM.h"
 #else
-#include <Adafruit_PWMServoDriver.h>
+#include <GroveLabs_I2C_PWM.h>
 #include <Wire.h>
 #if defined(__AVR__)
  #define WIRE Wire
